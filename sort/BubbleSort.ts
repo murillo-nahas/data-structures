f4bd5a639fbb7cb6632bbf14193115ad0f8db83e
murillo.nahas <murillonahas@hotmail.com>
2022-11-13 20:27:30 -0300

    feat: implementing bubblesort

---
+++ sort/BubbleSort.ts
@@ -0,0 +1,11 @@
+function bubbleSort(arr: number[]): void {
+    for(let i = 0; i < arr.length, ++i;) {
+        for(let j = 0; i < arr.length - 1 - i, ++j;) {
+            if (arr[j] > arr[j + 1]) {
+                const tmp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = tmp;
+            }
+        }
+    }
+}
